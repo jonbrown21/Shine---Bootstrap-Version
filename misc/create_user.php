ae93f8bed9d0f7460c7278fb2c88648cea27fd20
Jon Brown <jon@grovetech.co>
2020-05-02 17:54:03 -0400

    Removed unused files

--- misc/create_user.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(__DIR__."/../includes/class.config.php");
-require_once(__DIR__."/../includes/class.auth.php");
-$auth = Auth::getAuth();
-echo $auth->createHashedPassword($_REQUEST['password']);
